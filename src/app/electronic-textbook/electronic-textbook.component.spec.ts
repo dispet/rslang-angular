527e7d5d360a9af4348633d0c9c7611269747da0
Pavel Karenda <korendos@gmail.com>
2021-03-25 21:52:56 +0300

    feat: add electronic book component as main-page component

---
+++ src/app/electronic-textbook/electronic-textbook.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ElectronicTextbookComponent } from './electronic-textbook.component';
+
+describe('ElectronicTextbookComponent', () => {
+  let component: ElectronicTextbookComponent;
+  let fixture: ComponentFixture<ElectronicTextbookComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ ElectronicTextbookComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ElectronicTextbookComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/electronic-textbook/electronic-textbook.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-electronic-textbook',
+  templateUrl: './electronic-textbook.component.html',
+  styleUrls: ['./electronic-textbook.component.scss']
+})
+export class ElectronicTextbookComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
