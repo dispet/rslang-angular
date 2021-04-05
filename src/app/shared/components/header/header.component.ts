4a26e7e6e0ab2071c22dd2f46675e2cc7d29c523
Pavel Karenda <korendos@gmail.com>
2021-04-06 00:33:47 +0300

    feat: realise registration and login functionality

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,10 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AuthService, LocalStorageService } from '../../services';
+import { Subscription } from 'rxjs';
 
 @Component({
-  selector: 'app-header',
+	selector: 'app-header',
 	templateUrl: './header.component.html',
 	styleUrls: ['./header.component.scss'],
 })
-export class HeaderComponent {
-	constructor() {}
+export class HeaderComponent implements OnInit, OnDestroy {
+	private authSubscription: Subscription;
+
+	isLogin = false;
+
+	constructor(private authService: AuthService, private localStorageService: LocalStorageService) {}
+
+	ngOnInit(): void {
+		const isAuth = Boolean(this.localStorageService.getToken());
+		this.authSubscription = this.authService.isAuthSubject.subscribe((userStatus) => {
+			this.isLogin = userStatus || isAuth;
+		});
+	}
+
+	ngOnDestroy() {
+		this.authSubscription.unsubscribe();
+	}
+
+	logout(): void {
+		this.authService.isAuthSubject.next(false);
+		this.authService.logout();
+	}
 }
--- src/app/shared/services/auth.service.ts
+++ src/app/shared/services/auth.service.ts
@@ -1,84 +1,90 @@
-import {Injectable} from '@angular/core';
-import {HttpClient} from '@angular/common/http';
-import {Router} from '@angular/router';
-import {Observable} from 'rxjs';
-import {tap} from 'rxjs/operators';
-import {LocalStorageService} from './localStorage.service';
-import {UserService} from './user.service';
-import {ApiService} from './api.service';
-import {ILoginResponse, IRefreshTokenResponse, IUserCreate, IUserCreateResponse} from '../models';
-import {API_URL} from '../constants'
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Router } from '@angular/router';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { LocalStorageService } from './localStorage.service';
+import { UserService } from './user.service';
+import { ApiService } from './api.service';
+import { ILoginResponse, IRefreshTokenResponse, IUserCreate, IUserCreateResponse } from '../models';
+import { API_URL } from '../constants';
 
 @Injectable({
-  providedIn: 'root',
+	providedIn: 'root',
 })
 export class AuthService {
-  private url: string = API_URL;
-  private token: string = '';
-  private refreshToken: string = '';
+	private url: string = API_URL;
+	private token: string = '';
+	private refreshToken: string = '';
 
-  constructor(
-    private http: HttpClient,
-    private localStorageService: LocalStorageService,
-    private userService: UserService,
-    private router: Router,
-    private apiService: ApiService,
-  ) {
-  }
+	isAuthSubject = new BehaviorSubject<boolean>(false);
 
-  login(user: IUserCreate): Observable<ILoginResponse> {
-    return this.http.post<ILoginResponse>(`${this.url}/signin`, user).pipe(
-      tap(({refreshToken, token, userId}) => {
-        this.localStorageService.setAuthData({refreshToken, token, userId});
-        this.setToken(token);
-        this.setRefreshToken(refreshToken);
-        this.apiService.setUserId(userId);
-      })
-    );
-  }
+	constructor(
+		private http: HttpClient,
+		private localStorageService: LocalStorageService,
+		private userService: UserService,
+		private router: Router,
+		private apiService: ApiService,
+	) {}
 
-  register(user: IUserCreate): Observable<IUserCreateResponse> {
-    return this.http.post<IUserCreateResponse>(`${this.url}/users`, user);
-  }
+	login(user: IUserCreate): Observable<ILoginResponse> {
+		return this.http.post<ILoginResponse>(`${this.url}/signin`, user).pipe(
+			tap(({ refreshToken, token, userId }) => {
+				this.localStorageService.setAuthData({ refreshToken, token, userId });
+				this.setToken(token);
+				this.setRefreshToken(refreshToken);
+				this.apiService.setUserId(userId);
+			}),
+		);
+	}
 
-  updateTokens(): Observable<IRefreshTokenResponse> {
-    const userId = this.localStorageService.getUserId();
-    return this.http.get<IRefreshTokenResponse>(`${this.url}/users/${userId}/tokens`).pipe(
-      tap(({refreshToken, token}) => {
-        this.localStorageService.setAuthData({refreshToken, token, userId});
-        this.setToken(token);
-        this.setRefreshToken(refreshToken);
-      })
-    );
-  }
+	register(user: IUserCreate): Observable<IUserCreateResponse> {
+		return this.http.post<IUserCreateResponse>(`${this.url}/users`, user);
+	}
 
-  isAuthenticated(): boolean {
-    return Boolean(this.token);
-  }
+	updateTokens(): Observable<IRefreshTokenResponse> {
+		const userId = this.localStorageService.getUserId();
+		return this.http.get<IRefreshTokenResponse>(`${this.url}/users/${userId}/tokens`).pipe(
+			tap(({ refreshToken, token }) => {
+				this.localStorageService.setAuthData({ refreshToken, token, userId });
+				this.setToken(token);
+				this.setRefreshToken(refreshToken);
+			}),
+		);
+	}
 
-  setToken(token): void {
-    this.token = token;
-  }
+	isAuthenticated(): boolean {
+		return Boolean(this.localStorageService.getToken());
+	}
 
-  getToken(): string {
-    return this.token;
-  }
+	getAuthStatus(): void {
+		const isAuth = this.isAuthenticated();
+		isAuth ? this.isAuthSubject.next(true) : this.isAuthSubject.next(false);
+	}
 
-  setRefreshToken(refreshToken): void {
-    this.refreshToken = refreshToken;
-  }
+	setToken(token): void {
+		this.token = token;
+	}
 
-  getRefreshToken(): string {
-    return this.refreshToken;
-  }
+	getToken(): string {
+		return this.token;
+	}
 
-  logout(): void {
-    this.setToken(null);
-    this.setRefreshToken(null);
-    this.userService.setUser(null);
-    this.apiService.setUserId(null);
-    this.localStorageService.deleteUser();
-    this.localStorageService.clearAuthData();
-    this.router.navigate(['/login']);
-  }
+	setRefreshToken(refreshToken): void {
+		this.refreshToken = refreshToken;
+	}
+
+	getRefreshToken(): string {
+		return this.refreshToken;
+	}
+
+	logout(): void {
+		this.setToken(null);
+		this.setRefreshToken(null);
+		this.userService.setUser(null);
+		this.apiService.setUserId(null);
+		this.localStorageService.deleteUser();
+		this.localStorageService.clearAuthData();
+		this.router.navigate(['/login']);
+	}
 }
