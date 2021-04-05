04d806c778747765e0c92b77abad2c702593daff
Pavel Karenda <korendos@gmail.com>
2021-04-06 00:31:07 +0300

    feat: add login component

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,69 +1,61 @@
 import { Component, OnInit } from '@angular/core';
-import {FormControl, FormGroup, Validators} from "@angular/forms";
-import {ActivatedRoute, Params, Router} from "@angular/router";
-import {AuthService} from "../../shared/services";
-import {IUserCreate} from "../../shared/models";
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { ActivatedRoute, Params, Router } from '@angular/router';
+import { AuthService } from '../../shared/services';
+import { IUserCreate } from '../../shared/models';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+	form: FormGroup;
+	submitted = false;
+	message: string;
 
-  form: FormGroup;
-  submitted = false;
-  message: string;
+	constructor(public auth: AuthService, private router: Router, private route: ActivatedRoute) {}
 
-  constructor(
-    public auth: AuthService,
-    private router: Router,
-    private route: ActivatedRoute,
-  ) { }
+	ngOnInit(): void {
+		// if you are not logged in and try to go to dashboard,
+		// you get in url query param loginAgain=true.
+		// Then get this url like observable and check this queryParam
+		this.route.queryParams.subscribe((params: Params) => {
+			if (params.loginAgain) {
+				this.message = 'Please, login';
+			} else if (params.authFailed) {
+				this.message = 'Session is expired. Please login again';
+			}
+		});
 
-  ngOnInit(): void {
-    // if you are not logged in and try to go to dashboard,
-    // you get in url query param loginAgain=true.
-    // Then get this url like observable and check this queryParam
-    this.route.queryParams.subscribe((params: Params) => {
-      if (params.loginAgain) {
-        this.message = 'Please, login';
-      } else if (params.authFailed) {
-        this.message = 'Session is expired. Please login again'
-      }
-    });
+		this.form = new FormGroup({
+			email: new FormControl(null, [Validators.email, Validators.required]),
+			password: new FormControl(null, [Validators.required, Validators.minLength(6)]),
+		});
+	}
 
-    this.form = new FormGroup({
-      email: new FormControl(null, [
-        Validators.email,
-        Validators.required
-      ]),
-      password: new FormControl(null, [
-        Validators.required,
-        Validators.minLength(6)
-      ]),
-    });
-  }
+	submit(): void {
+		if (this.form.invalid) {
+			return;
+		}
 
-  submit(): void {
-    if (this.form.invalid) {
-      return;
-    }
+		// to disable button 'submit' if form was already submitted
+		this.submitted = true;
 
-    // to disable button 'submit' if form was already submitted
-    this.submitted = true;
+		// create user, get user data from UI
+		const user: IUserCreate = {
+			email: this.form.value.email,
+			password: this.form.value.password,
+		};
 
-    // create user, get user data from UI
-    const user: IUserCreate = {
-      email: this.form.value.email,
-      password: this.form.value.password,
-    };
-
-    // clean form fields and redirect to admin page
-    this.auth.login(user).subscribe(() => {
-        this.form.reset();
-        this.submitted = false;
-      },
-      () => this.submitted = false);
-  }
+		// clean form fields and redirect to admin page
+		this.auth.login(user).subscribe(
+			() => {
+				this.form.reset();
+				this.submitted = false;
+				this.router.navigate(['text-book']);
+			},
+			() => (this.submitted = false),
+		);
+	}
 }
