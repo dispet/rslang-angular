62d1ac3b0d15c0786e9d75f2b08878152913f0a6
Bury Vitali <vitalisbayer@gmail.com>
2021-04-07 21:49:29 +0300

    bug: change group is not working

--- src/app/state/state-facade.service.ts
+++ src/app/state/state-facade.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from "@angular/core";
-import { forkJoin, Observable } from "rxjs";
+import { Observable } from "rxjs";
 import { ApiService } from "../shared";
 import { Group, Page } from "../shared/types";
 import { StateService } from "./state.service";
@@ -12,25 +12,18 @@ import { IWord } from "../shared/models";
 export class FacadeService {
   constructor(private stateService: StateService, private apiService: ApiService) { }
 
-  readonly groupNumber$ = this.stateService.groupNumber$;
-  readonly pageNumber$ = this.stateService.pageNumber$;
+  readonly group = this.stateService.groupNumber$;
+  readonly page = this.stateService.pageNumber$;
   readonly words$ = this.stateService.words$;
 
-  // setGroupNumber(number: Group) {
-  //   this.stateService.setGroupNumber(number);
-  // }
+  readonly FIRST_PAGE = 0;
+  readonly LAST_PAGE = 29;
 
-  pickGroup(number: Group) {
-    let prevGroup: Group = this.stateService.getGroupNumberValue();
+  setGroup(number: number): void {
     this.stateService.setGroupNumber(number);
-    this.loadWords()
-    .pipe(tap({
-      next: words => this.stateService.setWords(words),
-      error: error => {
-        console.log("Can't load words", error.message);
-        this.stateService.setGroupNumber(prevGroup);
-      }
-    }));
+    // let prevGroup: Group = this.stateService.getGroupNumberValue();
+    // this.stateService.setGroupNumber(number);
+
     // this.stateService.loadWords()
     //   .pipe(tap({
     //     next: words => this.stateService.setWords(words),
@@ -41,24 +34,26 @@ export class FacadeService {
     //   }))
   }
 
+  setNextPage() {
+    this.stateService.setNextPageNumber();
+  }
 
+  setPrevPage() {
+    this.stateService.setPrevPageNumber();
+  }
 
-  // setPageNumber(number: Page) {
-  //   this.stateService.setPageNumber(number);
-  // }
+  setFirstPage() {
+    this.stateService.setPageNumber(this.FIRST_PAGE as Page);
+  }
 
-  pickPage(number: Page) {
-    let prevPage: Page = this.stateService.getPageNumberValue();
-    this.stateService.setPageNumber(number);
-    // this.stateService.setWords(this.loadWords())
-    this.loadWords()
-      .pipe(tap({
-        next: words => this.stateService.setWords(words),
-        error: error => {
-          console.log("Can't load words", error.message);
-          this.stateService.setPageNumber(prevPage);
-        }
-      }));
+  setLastPage() {
+    this.stateService.setPageNumber(this.LAST_PAGE as Page);
+  }
+
+  // pickPage(number: Page) {
+    // let prevPage: Page = this.stateService.getPageNumberValue();
+    // this.stateService.setPageNumber(number);
+
     // this.stateService.loadWords()
     //   .pipe(tap({
     //     next: words => this.stateService.setWords(words),
@@ -67,15 +62,21 @@ export class FacadeService {
     //       this.stateService.setPageNumber(prevPage);
     //     }
     //   }))
-  }
+  // }
 
   loadWords(): Observable<Array<IWord>> {
-    return forkJoin([
-      this.groupNumber$, this.pageNumber$
-    ]).pipe(switchMap(([
-      group, page
-    ]) => {
-      return this.apiService.getWords(group, page);
-    }));
+    return this.stateService.pagination$.pipe(
+      switchMap(({ page, group }) => {
+        return this.apiService.getWords(group, page);
+      }),
+      tap({
+        next: (data) => {
+          this.stateService.setWords(data);
+        },
+        error: (err) => {
+          console.error(JSON.stringify(err));
+        },
+      }),
+    );
   }
 }
