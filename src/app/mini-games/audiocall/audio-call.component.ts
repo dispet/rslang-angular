7816e03d17aacae1f6b3c3952f2fdd6c2d6d761e
Kutsak-Nataly <73638937+Kutsak-Nataly@users.noreply.github.com>
2021-04-13 15:22:23 +0300

    feat: work with a basic set of phrases without a user

--- src/app/mini-games/audiocall/audio-call.component.ts
+++ src/app/mini-games/audiocall/audio-call.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { IWord } from '../../shared/models';
 import { ApiService } from '../../shared/services';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-audio-call',
   templateUrl: './audio-call.component.html',
   styleUrls: ['./audio-call.component.scss'],
 })
-export class AudioCallComponent implements OnInit {
-  private maxWords = 7;
-  VariantsRu: string[] = ['один', 'два', 'три'];
+export class AudioCallComponent implements OnInit, OnDestroy {
+  private subscription: Subscription;
+  private variantsMax = 4;
+  colorTub = '#22ca06';
+  maxWords = 7;
+  resultCounter = 0;
+  variantsRuAll: string[][];
   allVariantsRu: string[];
   words: IWord[];
 
@@ -19,15 +24,21 @@ export class AudioCallComponent implements OnInit {
     this.getWords();
   }
 
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe();
+  }
+
   getWords() {
-    this.apiService.getWords(1, 1).subscribe(
+    this.subscription = this.apiService.getWords(1, 1).subscribe(
       (words) => {
         this.words = this.getRandomWords(words, this.maxWords);
         this.allVariantsRu = this.getAllVariantsRu(words);
+        this.variantsRuAll = this.getVariantsRu(this.words, this.allVariantsRu, this.variantsMax);
       },
       (error) => console.error(error),
     );
   }
+
   getRandomWords(arr: IWord[], n: number): IWord[] {
     let result: IWord[] = [],
       len = arr.length;
@@ -50,5 +61,31 @@ export class AudioCallComponent implements OnInit {
       collectionWords.push(words[i].wordTranslate);
     }
     return collectionWords;
+  }
+
+  getVariantsRu(words: IWord[], allVariantsRu: string[], varNum: number) {
+    let len = allVariantsRu.length;
+    let resultArr: Array<string[]> = [];
+    for (let i = 0; i < words.length; i++) {
+      let result: string[] = [];
+      result.push(words[i].wordTranslate);
+      let n = varNum;
+      while (n - 1) {
+        let x = Math.floor(Math.random() * len);
+        if (!result.includes(allVariantsRu[x])) {
+          result.push(allVariantsRu[x]);
+          n--;
+        }
+      }
+      result.sort(function () {
+        return Math.random() - 0.5;
+      });
+      resultArr.push(result);
+    }
+    return resultArr;
+  }
+
+  getAnswer(n: number) {
+    this.resultCounter += n;
   }
 }
--- src/app/mini-games/audiocall/item-audio-call/item-audio-call.component.ts
+++ src/app/mini-games/audiocall/item-audio-call/item-audio-call.component.ts
@@ -1,21 +1,43 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { IWord } from '../../../shared/models';
 
 @Component({
   selector: 'app-item-audio-call',
   templateUrl: './item-audio-call.component.html',
   styleUrls: ['./item-audio-call.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ItemAudioCallComponent implements OnInit {
-  wordsVariants: string[] = [];
-  @Input() variantsMax = 4;
-  @Input() VariantsRu: string[];
+  private url = 'https://dispet.github.io/rslang-data/';
+  disabledButton = false;
+  correctAnswer = '';
+  val: number;
+  @Input() getVariantsRu: string[];
   @Input() word: IWord;
-  @Output() result = new EventEmitter<IWord>();
+  @Output() answer = new EventEmitter<number>();
 
   constructor() {}
 
-  ngOnInit(): void {
-    this.wordsVariants[0] = this.word.wordTranslate;
+  ngOnInit(): void {}
+
+  playAudio(url1: string): void {
+    const audio = new Audio();
+    audio.src = this.url + url1;
+    audio.load();
+    audio.play();
+    audio.addEventListener('ended', function () {
+      if (audio.duration === audio.currentTime) {
+        audio.play();
+        audio.pause();
+        audio.currentTime = 0.0;
+      }
+    });
+  }
+
+  sendResult(v: string) {
+    this.val = v === this.word.wordTranslate ? 1 : 0;
+    this.disabledButton = true;
+    this.correctAnswer = this.word.wordTranslate;
+    this.answer.emit(this.val);
   }
 }
